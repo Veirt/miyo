2360fdd577136239ab5f2ae820975ea005c20930
Veirt <veirt@duck.com>
2024-02-16 16:18:56 +0800

    fix: change backend proxy url

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:9452",
         changeOrigin: true,
       },
     },
